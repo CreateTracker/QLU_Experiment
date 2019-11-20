7b60c73d09e77d2ab62f6a1f367ee80531ff53b8
CreateTracker <45835949+CreateTracker@users.noreply.github.com>
2019-11-20 19:39:47 +0800

    实验一

---
+++ experiment_1/experiment_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace experiment_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("学号：201803090028");
+            Console.WriteLine("姓名：201803090028");
+            Console.WriteLine("专业信息：计算机科学与技术（软件外包）");
+        }
+    }
+}
---
+++ experiment_1/experiment_1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace experiment_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+            str = "学号："+textBox1.Text + "\n姓名：" + textBox2.Text + "\n性别：" + textBox3.Text + "\n年龄：" + textBox4.Text + "\n专业：" + textBox5.Text + "\n";
+            textBox6.Text = str;
+        }
+    }
+}
